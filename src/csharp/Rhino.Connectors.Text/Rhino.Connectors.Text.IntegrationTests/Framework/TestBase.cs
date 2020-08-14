afb9020e271debe36a3aebc145effa443dadddf0
roei sabag <s_roe@DESKTOP-G1MC8H7.(none)>
2020-08-14 14:18:34 +0300

    fix integration tests

--- src/csharp/Rhino.Connectors.Text/Rhino.Connectors.Text.IntegrationTests/Framework/TestBase.cs
+++ src/csharp/Rhino.Connectors.Text/Rhino.Connectors.Text.IntegrationTests/Framework/TestBase.cs
@@ -5,13 +5,14 @@
  */
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Rhino.Api.Simulator.Framework;
-
-using System.IO;
-
 namespace Rhino.Connectors.Text.IntegrationTests.Framework
 {
     [TestClass]
+    [DeploymentItem("Resources/FullSpecJson.txt")]
+    [DeploymentItem("Resources/InputsModel.txt")]
+    [DeploymentItem("Resources/ModelsNoDataSource.txt")]
+    [DeploymentItem("Resources/SpecNoDataSourceIntegration.txt")]
+    [DeploymentItem("Resources/TablesModel.txt")]
     public abstract class TestBase
     {
         /// <summary>
@@ -19,28 +20,5 @@ namespace Rhino.Connectors.Text.IntegrationTests.Framework
         /// for Rhino.Connectors.Text.UnitTests.Framework.TestBase
         /// </summary>
         public TestContext TestContext { get; set; }
-
-        [AssemblyInitialize]
-        public static void Setup(TestContext context)
-        {
-            // write files
-            File.WriteAllText("ModelsNoDataSource.txt", Utilities.GetStub(name: "ModelsNoDataSource"));
-            File.WriteAllText("FullSpecJson.txt", Utilities.GetStub(name: "FullSpecJson"));
-            File.WriteAllText("InputsModel.txt", RhinoModelStub.InputsModel);
-            File.WriteAllText("TablesModel.txt", RhinoModelStub.TablesModel);
-
-            // log
-            context.WriteLine("Class setup completed");
-        }
-
-        [AssemblyCleanup]
-        public static void Cleanup()
-        {
-            // clean files
-            File.Delete("ModelsNoDataSource.txt");
-            File.Delete("FullSpecJson.txt");
-            File.Delete("InputsModel.txt");
-            File.Delete("TablesModel.txt");
-        }
     }
 }
